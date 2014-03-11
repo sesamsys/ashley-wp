49f5263f8d3093031ed21ed4334c9a32ea8c4cf6
sesam <petersz@me.com>
2014-03-11 12:00:06 +0100

    switched to Codrops sliding menu

--- footer.php
+++ footer.php
@@ -2,5 +2,19 @@
 	<footer class="small wrap">
 		<p class="small">Ashley-WP theme by <a href="http://sesam.hu">sesam</a></p>
 	</footer>
+<!-- Classie - class helper functions by @desandro https://github.com/desandro/classie -->
+<script src="<?php bloginfo( 'template_url' ); ?>/classie.js"></script>
+<script>
+	var 
+		menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+		showLeftPush = document.getElementById( 'showLeftPush' ),
+		body = document.body;
+
+	showLeftPush.onclick = function() {
+		classie.toggle( this, 'active' );
+		classie.toggle( body, 'cbp-spmenu-push-toright' );
+		classie.toggle( menuLeft, 'cbp-spmenu-open' );
+	};
+</script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,14 +5,24 @@
         <title><?php wp_title(''); ?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_url' ); ?>/component.css" />
+        <script src="<?php bloginfo( 'template_url' ); ?>/modernizr.custom.js"></script>
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <meta name="text:GoogleWebFont" content="Gentium Book Basic"/>
     	<meta name="text:GoogleWebFontURL" content="Gentium+Book+Basic"/>
         <link href='http://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,700,400italic,700italic&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
         <?php wp_head(); ?>
     </head>
-    <body <?php body_class(); ?>>
+    <body <?php body_class('cbp-spmenu-push'); ?>>
+    	<div class="menuicon">
+    		<div class="button" id="showLeftPush" >
+    		<h1>
+    			<a href="#"><i class="fa fa-bars"></i></a>
+    		</h1>
+    		</div>
+    	</div>
 		<header class="header">
 			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
@@ -20,7 +30,7 @@
 			</a>
 		</header>
 		<div id="navbar" class="navbar">
-			<nav id="site-navigation" class="navigation main-navigation wrap" role="navigation">
+			<nav id="cbp-spmenu-s1" class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" role="navigation">
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 			</nav>
 		</div>
