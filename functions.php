3675de07c9c9e3b468ab106c15103eef667f70f6
sesam <petersz@me.com>
2014-03-17 13:30:16 +0100

    style fixes and smarter commenting

--- functions.php
+++ functions.php
@@ -19,13 +19,16 @@ function ashley_comment($comment, $args, $depth) {
    $GLOBALS['comment'] = $comment; ?>
    <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
      <div id="comment-<?php comment_ID(); ?>">
-      <span>
+      <div>
       	 <?php echo get_avatar($comment,$size='40'); ?>
-      </span>
-      <span class="comment-text">
+      </div>
+      <div class="reply small">
+          <?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+        </div>
+      <div class="comment-text">
          <?php echo get_comment_author_link(); ?> on <a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php printf(__('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'  ','') ?>
       	 <?php echo comment_text() ?>
-      </span>
+      </div>
      </div>
 	<?php  
 }
