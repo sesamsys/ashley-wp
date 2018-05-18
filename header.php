0f63b61829292381c6f0f9d26b349bae7cef78fb
sesamsys <petersz@me.com>
2018-05-18 18:09:45 +0200

    Google Fonts HTTPS fix

--- header.php
+++ header.php
@@ -8,8 +8,8 @@
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <meta name="text:GoogleWebFont" content="Gentium Book Basic" />
     	<meta name="text:GoogleWebFontURL" content="Gentium+Book+Basic" />
-        <link href='http://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,700,400italic,700italic&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css' />
-        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css' />
+        <link href='https://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,700,400italic,700italic&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css' />
+        <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css' />
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
         <link rel="alternate" type="application/rss+xml" href="<?php bloginfo( 'rss2_url' ); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
